b54fa6e9ee9ac360c46a1b1620ff2e64e0e10cc6
NukaPepsiMan <davidelicone@hotmail.it>
2026-03-08 00:47:03 +0100

    setup rules security filter conf

---
+++ src/main/java/aruba/cloud/gamma/config/SecurityFilterChainConfig.java
@@ -0,0 +1,33 @@
+package aruba.cloud.gamma.config;
+
+import org.springframework.boot.security.autoconfigure.actuate.web.servlet.EndpointRequest;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityFilterChainConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChainConfig(HttpSecurity http) {
+        http.csrf(AbstractHttpConfigurer::disable);
+
+        http.authorizeHttpRequests(authorizeRequests
+                -> authorizeRequests.requestMatchers(EndpointRequest.toAnyEndpoint()).permitAll()
+                                    .anyRequest().authenticated());
+
+        http.sessionManagement(session
+                -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+
+        http.oauth2ResourceServer(oauth2
+                -> oauth2.jwt(Customizer.withDefaults()));
+
+        return http.build();
+    }
+}
