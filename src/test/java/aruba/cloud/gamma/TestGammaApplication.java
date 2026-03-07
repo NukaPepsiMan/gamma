4afc051b0159d8cca585fe220fd15342daa0a8f6
NukaPepsiMan <davidelicone@hotmail.it>
2026-03-08 00:27:37 +0100

    clean init

--- src/test/java/aruba/cloud/gamma/TestGammaApplication.java
+++ src/test/java/aruba/cloud/gamma/TestGammaApplication.java
@@ -4,8 +4,8 @@ import org.springframework.boot.SpringApplication;
 
 public class TestGammaApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.from(GammaApplication::main).with(TestcontainersConfiguration.class).run(args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.from(GammaApplication::main).run(args);
+    }
 
 }
--- src/test/java/aruba/cloud/gamma/TestcontainersConfiguration.java
+++
@@ -1,8 +0,0 @@
-package aruba.cloud.gamma;
-
-import org.springframework.boot.test.context.TestConfiguration;
-
-@TestConfiguration(proxyBeanMethods = false)
-class TestcontainersConfiguration {
-
-}
